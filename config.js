3c9bb52672e8244fe8e631af28a4d5e0bb122ff5
lota20034 <se.sokhnamy6@gmail.com>
2026-09-03 12:38:56 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Configurazione Supabase — Dahira Touba Fano
+const SUPABASE_URL = 'https://elgzwgudzwqrrzygfpkp.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_5rREEvJSaB4fLXNLSabFPQ_ooWsJEWH';
+const sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
